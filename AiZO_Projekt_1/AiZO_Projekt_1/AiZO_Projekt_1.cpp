3df79f4a2b89069b2daf26a8a108c5f1f4c9cd06
PoProstuFilip14 <f.romanowski@hotmail.com>
2026-03-27 22:07:13 +0100

    quick implementation of quicksort

---
+++ AiZO_Projekt_1/AiZO_Projekt_1/AiZO_Projekt_1.cpp
@@ -0,0 +1,79 @@
+﻿#include <iostream>
+
+int N = 100;
+
+template <typename T>
+void quickSort(T* array, int left, int right) {
+    int helper;
+    helper = array[left + ((right - left) / 2)];
+    array[left + ((right - left) / 2)] = array[right];
+    array[right] = helper;
+
+    int j = left;
+    for (int i = left; i < right; i++) {
+        if (array[i] < array[right]) {
+            helper = array[i];
+            array[i] = array[j];
+            array[j] = helper;
+            j++;
+        }
+    }
+
+    helper = array[right];
+    array[right] = array[j];
+    array[j] = helper;
+
+    /*for (int i = 0; i < N; i++) {
+        std::cout << array[i] << " ";
+    }
+    std::cout << std::endl;*/
+
+    if (j - 1 > left) {
+        quickSort(array, left, j - 1);
+    }
+
+    if (j + 1 < right) {
+        quickSort(array, j + 1, right);
+    }
+}
+
+int main()
+{
+    srand(time(0));
+
+    int* array = new int[N];
+    int* arrayCopy = new int[N];
+
+    for (int i = 0; i < N; i++) {
+        array[i] = rand() % 10;
+        arrayCopy[i] = array[i];
+    }
+    
+    for (int i = 0; i < N; i++) {
+        std::cout << array[i] << " ";
+    }
+    std::cout << std::endl;
+
+    quickSort(array, 0, N - 1);
+
+    for (int i = 0; i < N; i++) {
+        std::cout << array[i] << " ";
+    }
+    std::cout << std::endl;
+
+    for (int i = 0; i < N; i++) {
+        array[i] = arrayCopy[i];
+    }
+
+    for (int i = 0; i < N; i++) {
+        std::cout << array[i] << " ";
+    }
+    std::cout << std::endl;
+
+    quickSort(array, 0, N - 1);
+
+    for (int i = 0; i < N; i++) {
+        std::cout << array[i] << " ";
+    }
+    std::cout << std::endl;
+}
